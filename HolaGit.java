cb5b6a961ed2444ff18e7cd3c88718c6956bb7fc
joseporiolse-creator <joseporiolse@alumnes.ilerna.com>
2025-11-07 15:33:32 +0100

    Esta es mi rama cien por cien real no fake

--- HolaGit.java
+++ HolaGit.java
@@ -1,1 +1,1 @@
-public class HolaGit {public static void main(String[] args) {System.out.println("Hola soy calculaitor 2 ahora es personal");}} 
+public class HolaGit {public static void main(String[] args) {System.out.println("Hola soy calculaitor 2 ahora es personal"); System.out.println("Hola desde mi rama Josep"); }} 
